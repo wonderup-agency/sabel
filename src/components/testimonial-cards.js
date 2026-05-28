2c9a83f0c5867996acde19192440d1e6b79042a5
PabloWonderUp <144040322+PabloWonderUp@users.noreply.github.com>
2026-05-28 16:48:51 -0300

    Testimonials Slider

--- src/components/testimonial-cards.js
+++ src/components/testimonial-cards.js
@@ -3,92 +3,54 @@ Component: testimonial-cards
 Webflow attribute: data-component="testimonial-cards"
 */
 
+import Swiper from 'swiper'
+import { EffectCreative, A11y } from 'swiper/modules'
+import 'swiper/css'
+import 'swiper/css/effect-creative'
+import './testimonial-cards.css'
+
 /**
  * @param {HTMLElement[]} elements - All elements matching [data-component='testimonial-cards']
  */
 export default function (elements) {
-  const VISIBLE_COUNT = 3
-  const CYCLE_DURATION = 3.5
-  const SCALE_STEP = 0.1
-  const X_STEP = 40
-  const EXIT_SCALE = 1.05
-  const EXIT_DURATION = 0.6
-  const SHIFT_DURATION = 1
+  if (!elements.length) return
+
+  const instances = []
 
   elements.forEach((wrapper) => {
-    const cards = Array.from(
-      wrapper.querySelectorAll('.testimonials_card-item')
-    )
-    if (cards.length < 2) return
+    const slides = wrapper.querySelectorAll('.swiper-slide')
+    if (slides.length < 2) return
 
-    let order = cards.map((_, i) => i)
-
-    function positionCards(animate) {
-      order.forEach((cardIndex, stackPos) => {
-        const card = cards[cardIndex]
-        const isVisible = stackPos < VISIBLE_COUNT
-
-        const scale = isVisible
-          ? 1 - stackPos * SCALE_STEP
-          : 1 - VISIBLE_COUNT * SCALE_STEP
-        const x = isVisible ? stackPos * X_STEP : VISIBLE_COUNT * X_STEP
-        const zIndex = cards.length - stackPos
-        const opacity = isVisible ? 1 : 0
-
-        if (animate) {
-          gsap.to(card, {
-            scale,
-            x,
-            opacity,
-            duration: SHIFT_DURATION,
-            ease: 'power2.out',
-          })
-        } else {
-          gsap.set(card, { scale, x, opacity })
-        }
-
-        card.style.zIndex = zIndex
-      })
-    }
-
-    positionCards(false)
-
-    function cycle() {
-      const frontCardIndex = order[0]
-      const frontCard = cards[frontCardIndex]
-
-      frontCard.style.zIndex = cards.length + 1
-
-      gsap.to(frontCard, {
-        scale: EXIT_SCALE,
-        opacity: 0,
-        duration: EXIT_DURATION,
-        ease: 'power2.in',
-        onComplete() {
-          order.push(order.shift())
-          gsap.set(frontCard, {
-            scale: 1 - VISIBLE_COUNT * SCALE_STEP,
-            x: VISIBLE_COUNT * X_STEP,
-          })
-          positionCards(true)
+    const swiper = new Swiper(wrapper, {
+      modules: [EffectCreative, A11y],
+      effect: 'creative',
+      creativeEffect: {
+        limitProgress: 3,
+        prev: {
+          translate: [36, 8, -80],
+          scale: 0.9,
+          origin: '100% 50%',
         },
-      })
-    }
-
-    let intervalId = setInterval(cycle, CYCLE_DURATION * 1000)
-
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        if (entry.isIntersecting) {
-          if (!intervalId)
-            intervalId = setInterval(cycle, CYCLE_DURATION * 1000)
-        } else {
-          clearInterval(intervalId)
-          intervalId = null
-        }
+        next: {
+          translate: [36, 8, -80],
+          scale: 0.9,
+          origin: '100% 50%',
+        },
       },
-      { threshold: 0.1 }
-    )
-    observer.observe(wrapper)
+      loop: true,
+      grabCursor: true,
+      speed: 800,
+      a11y: {
+        enabled: true,
+      },
+    })
+
+    instances.push(swiper)
   })
+
+  return {
+    resize() {
+      instances.forEach((s) => s.update())
+    },
+  }
 }
